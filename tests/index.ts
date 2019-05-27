6f8f55e9310b3b5f2f43d67d5e99fbdb649f67a0
王文璐 <kingwenlu@gmail.com>
2019-05-27 13:36:20 +0800

    update test

--- tests/index.ts
+++ tests/index.ts
@@ -1,7 +1,10 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import Parser from "../src/io/parser";
+import Emitter from '../src/io/emitter';
 
 const buffer = fs.readFileSync("./tests/forin-001.binjs")
 const parser = new Parser(buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength))
-console.log(JSON.stringify(parser.parse()))
+const program = parser.parse()
+const emitter = new Emitter()
+emitter.emit(program)
